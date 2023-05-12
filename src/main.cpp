7e4b94c9005f02fbeab4e575286bc7c038a87093
jorgefz <jorgefz.fernandez@gmail.com>
2023-05-12 18:40:30 +0100

    Added loading program from filename given on command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,23 @@
 #include <cassert>
 
 
+void program_analyser(const std::string& filename, int max){
+    auto chip8 = CHIP8::Interpreter();
+    chip8.load_file(filename);
+    auto& ram = chip8.get_state().ram;
+}
+
 int main(int argc, const char* argv[]) {
 
+    if(argc != 2){
+        std::cout << 
+        "Supply filename" << std::endl;
+        return 1;
+    }
+    program_analyser(argv[1], 10);
+
     auto chip8 = CHIP8::Interpreter();
-    chip8.load_file("../test_games/snake.ch8");
+    chip8.load_file(argv[1]);
     chip8.run();
 
 }
